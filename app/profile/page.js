a0f86e5686681e0fecd00edc8c727d1dad192f8e
ibnu naufal <ibnunaufal5758@gmail.com>
2025-02-20 13:18:26 +0700

    fir error production

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,10 +1,10 @@
-import Avatar from '@/components/Avatar';
+// import Avatar from '@/components/Avatar';
 import React from 'react';
 
 const ProfilePage = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <Avatar />
+            {/* <Avatar /> */}
             <div className="bg-white p-6 rounded-lg shadow-lg">
                 <img
                     className="w-24 h-24 rounded-full mx-auto"
